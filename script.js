816e85e1925a919c5d840bc29a4aeb755251746c
tonysama-lol <78974029+tonysama-lol@users.noreply.github.com>
2022-08-10 23:32:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+function what_we_do() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("ajax").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "what-we-do.html");
+    xhttp.send();
+}
+
+function gallery() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("ajax").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "gallery.html");
+    xhttp.send();
+}
+
+function meet_our_team() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("ajax").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "meet-our-team.html");
+  xhttp.send();
+}
+
+function how_to_read_Yotsuba() {
+  window.scrollTo({ top: 450, behavior: 'smooth' })
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("ajax").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "how-to-read-Yotsuba.html");
+  xhttp.send();
+}
+
+function how_to_read_Yotsuba1() {
+  window.scrollTo({ top: 450, behavior: 'smooth' })
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("ajax").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "how-to-read-Yotsuba1.html");
+  xhttp.send();
+}
+
+function how_to_read_Yotsuba2() {
+  window.scrollTo({ top: 450, behavior: 'smooth' })
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("ajax").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "how-to-read-Yotsuba2.html");
+  xhttp.send();
+}
+
+function how_to_read_Yotsuba3() {
+  window.scrollTo({ top: 450, behavior: 'smooth' })
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("ajax").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "how-to-read-Yotsuba3.html");
+  xhttp.send();
+}
+
+function how_to_read_Yotsuba4() {
+  window.scrollTo({ top: 450, behavior: 'smooth' })
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("ajax").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "how-to-read-Yotsuba4.html");
+  xhttp.send();
+}
+
+function how_to_read_Yotsuba5() {
+  window.scrollTo({ top: 450, behavior: 'smooth' })
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("ajax").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "how-to-read-Yotsuba5.html");
+  xhttp.send();
+}
+
+function how_to_read_Yotsuba6() {
+  window.scrollTo({ top: 450, behavior: 'smooth' })
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("ajax").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "how-to-read-Yotsuba6.html");
+  xhttp.send();
+}
+
+function how_to_read_Yotsuba7() {
+  window.scrollTo({ top: 450, behavior: 'smooth' })
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("ajax").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "how-to-read-Yotsuba7.html");
+  xhttp.send();
+}
+
+function open_facebook(){
+  window.open("https://facebook.com/yotsubatodaily");
+}
+function open_discord(){
+  window.open("https://discord.gg/M2Py59CwyK");
+}
+function open_email(){
+  window.open("email.html");
+}
+function open_twitter(){
+  window.open("https://twitter.com/Yotsubatodaily");
+}
+function open_insta(){
+  window.open("https://www.instagram.com/yotsubatodaily/");
+}
